d237b376fbe9c91a76444c57a42560da937cb4cf
Neolaki <EnesArat.ae@gmail.com>
2017-01-24 23:26:14 +0300

    Create 1den10akadarSayilar.java

---
+++ 1den10akadarSayilar.java
@@ -0,0 +1,10 @@
+package javaapplication7;
+
+public class JavaApplication7 {
+  public static void main(String[] args) {
+       int i;
+      for(i=1;i<=10;i++)
+       System.out.println(i);
+          
+  }
+}
